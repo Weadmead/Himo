24ed702751602053130a765ddae3a7b048928d0c
Weadmead <wead758@gmail.com>
2022-10-18 02:51:41 +0300

    landing page complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
 
+    $('.bike__slider, .slider__items').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
 
+      
     
 });
